d9649b02a5cba69c70cc531806e1c5f3893d95e1
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2025-10-21 10:17:21 +0530

    feat: Create Django project and DocumentUpload app

---
+++ dip-backend/dip_backend/DocumentUpload/views.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here
+class Home(APIView):
+    def get(self, req, format=None):
+        return Response("<h1>Hi hello</h1>", status=status.HTTP_200_OK)
